eee9718ec72e76fef9b61bd8c94150a9c5554d22
P4RASTOO <132952512+P4RASTOO@users.noreply.github.com>
2023-09-28 08:50:15 -0400

    crypto_wallet.py

--- crypto_wallet.py
+++ crypto_wallet.py
@@ -58,23 +58,37 @@ def send_transaction(w3, account, to, wage):
     # Convert eth amount to Wei
     value = w3.toWei(wage, "ether")
 
+    # Set a fixed gas price
+    gas_price = w3.toWei(10, "gwei")  # 10 Gwei
+
     # Calculate gas estimate
     gasEstimate = w3.eth.estimateGas(
         {"to": to, "from": account.address, "value": value}
     )
 
+def send_transaction(w3, account, to, wage):
+    """Send an authorized transaction to the Ganache blockchain."""
+    # Convert eth amount to Wei
+    value = w3.toWei(wage, "ether")
+    
+    # Set a fixed gas price
+    gas_price = w3.toWei(10, "gwei")  # 10 Gwei
+    
+    # Calculate gas estimate
+    gas_estimate = w3.eth.estimate_gas({"to": to, "from": account.address, "value": value})
+    
     # Construct a raw transaction
     raw_tx = {
         "to": to,
         "from": account.address,
         "value": value,
-        "gas": gasEstimate,
-        "gasPrice": 0,
-        "nonce": w3.eth.getTransactionCount(account.address),
+        "gas": gas_estimate,
+        "gasPrice": gas_price,
+        "nonce": w3.eth.get_transaction_count(account.address)
     }
-
-    # Sign the raw transaction with ethereum account
-    signed_tx = account.signTransaction(raw_tx)
-
-    # Send the signed transactions
-    return w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    
+    # Sign the raw transaction with the Ethereum account
+    signed_tx = account.sign_transaction(raw_tx)
+    
+    # Send the signed transaction
+    return w3.eth.send_raw_transaction(signed_tx.rawTransaction)
